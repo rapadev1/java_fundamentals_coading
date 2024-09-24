e795e7217938916281f542f2037111b065444112
rapadev1 <cristian.rapa.rapa@gmail.com>
2024-09-24 19:14:30 +0300

    Ex with random numbers

--- src/main/java/org/example/random_generator/Main.java
+++ src/main/java/org/example/random_generator/Main.java
@@ -1,4 +1,22 @@
 package org.example.random_generator;
+import java.util.Random;
 
 public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int sum = 0;
+        int avrage = 0;
+        int product = 1;
+        for (int i = 0; i < 10; i++) {
+            int randomNumber = rand.nextInt(1, 10);
+            sum += randomNumber;
+            product *= randomNumber;
+
+        }
+        avrage = sum / 10;
+        System.out.println("Average is "+ avrage);
+        System.out.println("Sum is "+ sum);
+        System.out.println("Product is "+ product);
+    }
+
 }
